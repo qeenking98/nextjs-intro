b62351bfeffed0b7a6ad4e05e5d118d24f6d6d59
qeenking <qeenking@daum.net>
2022-06-16 17:17:00 +0900

    end of nextjs study

---
+++ pages/404.js
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return "what the hell are you doing here?";
+}
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Seo from "../components/Seo";
+
+export default function about() {
+  return (
+    <div>
+      <Seo title="about" />
+      <h1>about us</h1>
+    </div>
+  );
+}
